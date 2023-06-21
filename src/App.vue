202337d93c7f2ff99ba007d93976b6d3dca262df
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-06-21 17:48:34 +0200

    added and structured AppMain component

--- src/App.vue
+++ src/App.vue
@@ -11,20 +11,8 @@ export default{
 
 <template>
   <AppHeader/>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss">
+    @import './styles/general.scss';
 </style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+
+export default {
+    name:'AppMain',
+    components:{
+
+    },
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+
+    }
+}
+</script>
+<style lang="scss">
+    @use '../styles/partials/mixins' as*;
+    @use '../styles/partials/variables' as*;
+</style>
