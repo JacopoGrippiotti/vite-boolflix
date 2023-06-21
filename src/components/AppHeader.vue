1128d7f8e18f93a4e3515ec3f10a4e609b9dde98
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-06-21 17:25:55 +0200

    added and structured AppHeader component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+<script>
+export default {
+    name:AppHeader,
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+    
+</style>
