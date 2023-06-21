9ee3a0fd302b80b663ddfe62fdd59a4d553cef61
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-06-21 18:58:40 +0200

    added FilmAdder component and added FilmElement component as a child

---
+++ src/components/FilmAdder.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="films-container">
+        <FilmElement/>
+    </div>
+</template>
+
+<script>
+import FilmElement from './FilmElement.vue';
+export default {
+    name:'FilmAdder',
+    components:{
+        FilmElement
+    }
+}
+</script>
+<style lang="scss" scoped>
+    
+</style>
