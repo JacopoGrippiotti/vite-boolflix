76cb3922c6655e433fb72d3fc1dd9a73cf5c5223
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-06-21 18:54:32 +0200

    added AppMain component to App.vue and structured AppMain component with a method that calls a function at custom event

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,23 +1,31 @@
 <template>
     <main>
-        
+        <SearchBar @searchedFilm="newCall"/>
     </main>
 </template>
 
 <script>
-
+import SearchBar from './SearchBar.vue';
+import axios from 'axios';
 export default {
     name:'AppMain',
     components:{
-
+        SearchBar
     },
     data(){
         return{
-
+            apiUrl:'https://api.themoviedb.org/3/search/movie?api_key=4e1e3ce8a9a8b7f034096dab8c5b88cc&query=',
+            filmList:[]
         }
     },
     methods:{
-
+        newCall(filmString){
+            axios.get(this.apiUrl + filmString)
+            .then((response) => {
+                this.filmList = response.data.results
+                console.log(this.filmList)
+            })
+            } 
     }
 }
 </script>
