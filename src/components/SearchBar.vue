9e837ff9d30d8a91e25c58e17c6e25e4b6ada986
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-06-21 17:54:23 +0200

    added input and button to SearchBar component

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,11 +1,15 @@
 <template>
-    <div>
-        
-    </div>
+    <input type="text" placeholder="insert film here" v-model="searchValue">
+    <button @click="$emit('searchedFilm',searchValue)">search</button>
 </template>
 <script>
 export default {
     name:'SearchBar',
+    data(){
+        return{
+            searchValue:""
+        }
+    },
     methods:{
 
     }
